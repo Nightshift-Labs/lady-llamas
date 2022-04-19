1db5dec0bb50f622cac1e56107d43f33ef5c544e
dank <78863078+karpienia@users.noreply.github.com>
2022-04-18 20:32:02 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import dynamic from "next/dynamic";
 
+const Header = dynamic(() => import("../components/header"));
 const MintSection = dynamic(() => import("../components/mint-section"));
 
 const Home = () => {
   return (
     <>
+      <Header title="Mint | Silly Sushi by Foodgod" />
       <MintSection />
     </>
   );
