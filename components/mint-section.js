9a868cb965e3e460000d009936e671189b49a7f1
dbrags <91096301+dbrags@users.noreply.github.com>
2022-04-20 12:44:29 -0400

    updates to gif name

--- components/mint-section.js
+++ components/mint-section.js
@@ -7,10 +7,12 @@ import { FaSpinner } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
 import Image from "next/image";
+import Link from "next/link";
 
 import useMintContract from "../hooks/useMintContract";
 import { getOwnerNfts } from "../services/nft-service";
 import { WalletModalContext } from "../layout/page";
+import { Discord, Twitter, OpenSea } from "./icons";
 
 const TransactionModal = dynamic(() =>
   import("../components/transaction-modal")
@@ -446,7 +448,7 @@ const MintSection = () => {
                 <span className="outline-title"> MINT YOUR </span>LADY
                 <Image
                   className=""
-                  src="/images/Lady-LLamas.gif"
+                  src="/images/lady-llamas.gif"
                   alt="Lady-LLamas"
                   width="70"
                   height="79"
@@ -491,6 +493,63 @@ const MintSection = () => {
             </div>
           </div>
         </div>
+
+        <nav className="absolute inset-x-0 bottom-0 h-16 z-10 lg:hidden">
+          <div className="container flex justify-between items-center w-full">
+            <div>
+              <Link href="/">
+                <a title=""></a>
+              </Link>
+            </div>
+            <ul className=" flex flex-row items-center space-x-8">
+              <li>
+                <a
+                  className="flex flex-row text-lightPurple font-bold text-sm"
+                  href={process.env.OPENSEA_LINK}
+                  title="View on OpenSea"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <OpenSea />
+                  OPENSEA
+                </a>
+              </li>
+              <li>
+                <a
+                  className="flex flex-row text-lightPurple font-bold text-sm"
+                  href=""
+                  title="Follow Twitter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Twitter />
+                  TWITTER
+                </a>
+              </li>
+              <li>
+                <a
+                  className="flex flex-row text-lightPurple font-bold text-sm"
+                  href=""
+                  title="Join Discord"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Discord />
+                  DISCORD
+                </a>
+              </li>
+              <li className="text-lightPurple font-bold text-sm">
+                <a
+                  href=""
+                  title="Contract"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                />
+                CONTRACT
+              </li>
+            </ul>
+          </div>
+        </nav>
       </section>
       <TransactionModal
         isOpen={isTransactionModalOpen}
--- layout/page.js
+++ layout/page.js
@@ -4,7 +4,6 @@ import dynamic from "next/dynamic";
 export const WalletModalContext = createContext(null);
 
 const Navbar = dynamic(() => import("../components/navbar"));
-const Footer = dynamic(() => import("../components/footer"));
 const WalletModal = dynamic(() => import("../components/wallet-modal"));
 
 const Page = ({ children }) => {
@@ -25,7 +24,6 @@ const Page = ({ children }) => {
       >
         <Navbar />
         <main>{children}</main>
-        <Footer />
         <WalletModal />
       </WalletModalContext.Provider>
     </div>
